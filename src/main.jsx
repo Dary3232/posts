d20f7d7fb64b839a406579f12b3b0088c9363f1e
Darya Shkuratova <d.legascheva@gmail.com>
2025-02-12 13:34:27 +0300

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,53 @@
 import { render } from 'preact'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import './index.css'
 import { App } from './app.jsx'
+import { Posts } from './pages/posts';
+import { Root } from './components/Root';
+import { DetailPost } from './pages/posts/detail';
+import { EditPost } from './pages/posts/edit';
+import { AddPost } from './pages/posts/add';
+import { Auth } from './pages/auth';
+import { Registration } from './pages/registration';
 
-render(<App />, document.getElementById('app'))
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+            index: true,
+            element: <App />
+        },
+        {
+            path: 'posts',
+            element: <Posts />,
+        },
+        {
+            path: 'posts/:id',
+            element: <DetailPost />
+        },
+        {
+            path: 'posts/:id/edit',
+            element: <EditPost />
+        },
+        {
+            path: 'posts/add',
+            element: <AddPost />
+        },
+        {
+            path: 'auth',
+            element: <Auth />
+        },
+        {
+            path: 'registration',
+            element: <Registration />
+        }
+      ]
+    },
+  ]);
+
+render(<RouterProvider router={router} />, document.getElementById('app'))
